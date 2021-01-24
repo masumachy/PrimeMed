a2840432b3229ded54cae191cd631025d763c5e1
sukanta7660 <spurkayastha83@gmail.com>
2021-01-24 13:50:31 +0600

    add to cart added

---
+++ app/Http/Controllers/User/CartController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\User;
+use App\TempOrder;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\DB;
+
+class CartController extends Controller
+{
+    public function add_cart(Request $request){
+
+        $isExist = TempOrder::where('sessionId', Cookie::get('unique_session'))
+        ->where('product_id', $request->productID)->first();
+    
+            if($isExist == null){
+                $table = new TempOrder();
+                $table->sessionId = Cookie::get('unique_session');
+                $table->product_id = $request->productID;
+                $table->price = $request->price;
+                $table->quantity = 1;
+                $table->save();
+            }else{
+                $old_qty = $isExist->quantity;
+                TempOrder::where('sessionID', Cookie::get('unique_session'))
+                ->where('product_id', $request->productID)->update(['quantity' => ($old_qty + 1)]);
+            }
+    
+            return 0;
+    
+       }
+
+       public function remove_cart(Request $request){
+
+        $isExist = TempOrder::where('sessionId', Cookie::get('unique_session'))
+        ->where('product_id', $request->productID)->first();
+    
+        if($isExist != null){
+            $old_qty = $isExist->quantity;
+    
+            if($old_qty == 1){
+                TempOrder::where('sessionId', Cookie::get('unique_session'))
+                ->where('product_id', $request->productID)->delete();
+            }else{
+                TempOrder::where('sessionId', Cookie::get('unique_session'))
+                ->where('product_id', $request->productID)->update(['quantity' => ($old_qty - 1)]);
+            }
+        }
+    
+        return 0;
+    
+       }
+
+       public function get_temp_order(){
+        $table = TempOrder::where('sessionId', Cookie::get('unique_session'))->sum('quantity');
+        return $table;
+    }
+}
--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -6,6 +6,7 @@ use App\ProductCategory;
 use App\AllLab;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cookie;
 
 class HomeController extends Controller
 {
@@ -13,5 +14,20 @@ class HomeController extends Controller
         $depts = Department::all();
         $labs = AllLab::all();
         return view('user.home',compact('depts','labs'));
+    }
+
+    public function gen_session(){
+        if (!Cookie::get('unique_session')) {
+            Cookie::queue('unique_session', md5(date('Y-m-d H:i:s')), config('sp.cookie_time'));
+        }
+    }
+
+    public function remove_session(){
+
+        if (Cookie::get('unique_session')) {
+            Cookie::queue('unique_session', null, 0);
+        }
+
+        return redirect()->back();
     }
 }
